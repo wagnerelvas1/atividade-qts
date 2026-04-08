dbb1f4135c097993f2b76aac8cf536fc6baefaba
wagnerelvas1 <wagner.relvas@gmail.com>
2026-04-08 11:27:01 -0400

    coisas

--- backend/CadastrarEntrada.php
+++ backend/CadastrarEntrada.php
@@ -7,7 +7,21 @@ $qtd = $_POST["quantidade"];
 $data = $_POST["data"];
 
 $data_formatada = date("Ymd", strtotime($data));
-$cod = $data_formatada . $qtd . $produto;
+$qtd_formatada = $qtd;
+if (strlen($qtd_formatada) != 4) {
+  for ($i = strlen($qtd_formatada); $i < 4; $i++) {
+    $qtd_formatada = "0" . $qtd_formatada;
+  }
+}
+
+$sql = "SELECT cod FROM produtos WHERE id = :produto";
+$stmt = $conn->prepare($sql);
+$stmt->execute([
+  "produto" => $produto
+]);
+$produto_codigo = $stmt->fetchColumn();
+
+$cod = $data_formatada . $qtd_formatada . $produto_codigo;
 
 $sql = "INSERT INTO entradas (cod, fk_produto, qtd, data_entrada) VALUES (:cod, :produto, :qtd, :data_entrada)";
 $stmt = $conn->prepare($sql);
